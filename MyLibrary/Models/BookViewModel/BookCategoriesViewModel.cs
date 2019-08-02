030734afa38b31fb29bbb61c62ea8f0ffae919b7
selam <selam0315@gmail.com>
2019-08-01 21:08:27 -0500

    view model for book category, create and edit views

---
+++ MyLibrary/Models/BookViewModel/BookCategoriesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Models.BookViewModel
+{
+    public class BookCategoriesViewModel
+    {
+        public List<GroupedBooks> GroupedBooks { get; set; }
+        public Book book { get; set; }
+    }
+}
---
+++ MyLibrary/Models/BookViewModel/BookEditViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Models.BookViewModel
+{
+    public class BookEditViewModel
+    {
+        public Book Book { get; set; }
+        public List<Category> CategoryList { get; set; }
+        public List<Author> AvailableAuthors { get; set; }
+        public List<SelectListItem> CategoryOptions =>
+            CategoryList?.Select(a => new SelectListItem(a.CategoryName, a.CategoryId.ToString())).ToList();
+        public List<SelectListItem> AuthorOptions =>
+            AvailableAuthors?.Select(a => new SelectListItem(a.FullName, a.AuthorId.ToString())).ToList();
+    }
+}
