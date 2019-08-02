fa6706b11c54ab5ee0cca729f5d139a610dc7f59
selam <selam0315@gmail.com>
2019-08-01 20:59:14 -0500

    model to group books in a category

---
+++ MyLibrary/Models/GroupedBooks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Models
+{
+    public class GroupedBooks
+    {
+        public int TypeId { get; set; }
+        public string TypeName { get; set; }
+        public int BookCount { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
